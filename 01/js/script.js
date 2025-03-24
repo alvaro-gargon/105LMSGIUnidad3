66e75b5e47e564a19699bade388aab839baecfeb
Alvaro Garcia <alvaro.gargon.4@educa.jcyl.es>
2025-03-24 09:22:14 +0100

    Comienzo de js

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,2 +1,16 @@
+var titulo =document.getElementById("titulo")
 
+titulo.innerText="Este es el titulo";
 
+var parrafo =document.getElementsByClassName("parrafo");
+
+parrafo[0].innerText="Este es el 1 ";
+
+parrafo[1].outerHTML="<div>te hago un div </div>"
+
+var cuerpo =document.getElementById("cuerpo")
+
+cuerpo.style.backgroundColor="#f4f4f4"
+cuerpo.style.textAlign="center"
+titulo.style.color="#333"
+titulo.style
