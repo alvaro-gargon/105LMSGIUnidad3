d2db5c52186d78e654dd1012f4353a6de418ac9d
Alvaro Garcia <alvaro.gargon.4@educa.jcyl.es>
2025-04-09 13:53:23 +0200

    Ej8 en desarrollo

---
+++ 08/js/script.js
@@ -0,0 +1,7 @@
+function generarTabla(){
+    let tabla=document.createElement("table");
+}
+document.addEventListener("DOMContentLoaded", function(){
+    document.querySelector('input[type="button"][value="Genera una tabla"]').addEventListener("click", generarTabla);
+    } // fin de la funciÃ³n
+    ) // fin AddEventListener
